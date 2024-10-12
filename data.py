9de96b541ee726638f668739fbc2c85ed772710c
Loch <ana.26.62.maria.04@gmail.com>
2024-10-12 18:10:49 +0300

    excel->list

---
+++ data.py
@@ -0,0 +1,15 @@
+from openpyxl import load_workbook
+
+date = load_workbook("first_sheet_from_data.xlsx")
+sheet = date.active
+
+headers = [cell.value for cell in sheet[1]]
+data = []
+
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    row_dict = {header: (cell if cell is not None else "-") for header, cell in zip(headers, row)}
+    data.append(row_dict)
+
+print(data) # aici avem intr-o lista, fiecare linie din doc
+print(len(data))
+
