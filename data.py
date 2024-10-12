1cca2ac339364a4f0a328a5a17831957dc568792
Loch <ana.26.62.maria.04@gmail.com>
2024-10-12 19:34:58 +0300

    eliminated dublicates

--- data.py
+++ data.py
@@ -4,11 +4,22 @@ date = load_workbook("first_sheet_from_data.xlsx")
 sheet = date.active
 
 headers = [cell.value for cell in sheet[1]]
+
+filtered_headers = headers[2:]
+
 data = []
 
 for row in sheet.iter_rows(min_row=2, values_only=True):
-    row_dict = {header: (cell if cell is not None else "-") for header, cell in zip(headers, row)}
+    row_dict = {header: (cell if cell is not None else "-") for header, cell in zip(filtered_headers, row[2:])}
     data.append(row_dict)
 
-print(data) # aici avem intr-o lista, fiecare linie din doc
-print(len(data))
+print(data)
+print(f"lungime veche: {len(data)}")
+
+data_without_duplicates = []
+for i in data:
+    if i not in data_without_duplicates:
+        data_without_duplicates.append(i)
+
+print(f"lungime noua: {len(data_without_duplicates)}")
+
