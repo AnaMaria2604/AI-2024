4560b9e88a941b0c062a7f6fd39a19e2407469b1
Loch <ana.26.62.maria.04@gmail.com>
2025-01-13 15:57:47 +0200

    updates

--- data_types.py
+++ data_types.py
@@ -27,7 +27,12 @@ Race = [
 ]
 
 Number = [
-    ("nan", "Number of cats in the household", 0)
+    ("1", "one cat in the household", 1),
+    ("2", "two cats in the household", 2),
+    ("3", "three cats in the household", 3),
+    ("4", "four cats in the household", 4),
+    ("5", "five cats in the household", 5),
+    ("Plusde5", "more than five cats in the household", 0.0),
 ]
 
 Home = [
@@ -44,11 +49,11 @@ Zone = [
 ]
 
 Ext = [
-    ("1", "None", 0),
-    ("2", "Limited (less than one hour)", 1),
-    ("3", "Moderate (1 to 5 hours)", 2),
-    ("4", "Long (more than 5 hours)", 3),
-    ("5", "All the time (come back just to eat)", 4)
+    ("1", "no time outside", 0),
+    ("2", "less than one hour outside", 1),
+    ("3", "1 to 5 hours outside", 2),
+    ("4", "more than 5 hours outside", 3),
+    ("5", "All the time outside", 4)
 ]
 
 Obs = [
@@ -58,99 +63,126 @@ Obs = [
     ("4", "Long (more than 5 hours)", 3)
 ]
 
-Shy=[
-    ("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),
+Shy = [
+    ("1", "bold", 1.0),
+    ("2", "timid", 2.0),
+    ("3", "reserved", 3.0),
+    ("4", "introverted", 4.0),
+    ("5", "reclusive", 5.0)
 ]
 
-Calm=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Calm = [
+    ("1", "tense", 1.0),
+    ("2", "serene", 2.0),
+    ("3", "composed", 3.0),
+    ("4", "tranquil", 4.0),
+    ("5", "peaceful", 5.0)
+]
 
-Frightened=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Frightened = [
+    ("1", "brave", 1.0),
+    ("2", "anxious", 2.0),
+    ("3", "alarmed", 3.0),
+    ("4", "terrified", 4.0),
+    ("5", "petrified", 5.0)
+]
 
-Intelligent=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Intelligent = [
+    ("1", "dull", 1.0),
+    ("2", "smart", 2.0),
+    ("3", "clever", 3.0),
+    ("4", "bright", 4.0),
+    ("5", "brilliant", 5.0)
+]
 
-Vigilant=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Vigilant = [
+    ("1", "careless", 1.0),
+    ("2", "alert", 2.0),
+    ("3", "attentive", 3.0),
+    ("4", "watchful", 4.0),
+    ("5", "observant", 5.0)
+]
 
-Pervering=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Persevering = [
+    ("1", "lazy", 1.0),
+    ("2", "determined", 2.0),
+    ("3", "persistent", 3.0),
+    ("4", "dedicated", 4.0),
+    ("5", "resolute", 5.0)
+]
 
-Affectionate=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Affectionate = [
+    ("1", "cold", 1.0),
+    ("2", "warm", 2.0),
+    ("3", "loving", 3.0),
+    ("4", "tender", 4.0),
+    ("5", "devoted", 5.0)
+]
 
-Friendy=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Friendly = [
+    ("1", "hostile", 1.0),
+    ("2", "cordial", 2.0),
+    ("3", "kind", 3.0),
+    ("4", "pleasant", 4.0),
+    ("5", "amiable", 5.0)
+]
 
-Solitary=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Solitary = [
+    ("1", "social", 1.0),
+    ("2", "lonely", 2.0),
+    ("3", "isolated", 3.0),
+    ("4", "introverted", 4.0),
+    ("5", "reclusive", 5.0)
+]
 
-Brutal=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Brutal = [
+    ("1", "gentle", 1.0),
+    ("2", "rough", 2.0),
+    ("3", "harsh", 3.0),
+    ("4", "fierce", 4.0),
+    ("5", "savage", 5.0)
+]
 
-Dominant=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Dominant = [
+    ("1", "submissive", 1.0),
+    ("2", "controlling", 2.0),
+    ("3", "assertive", 3.0),
+    ("4", "commanding", 4.0),
+    ("5", "overbearing", 5.0)
+]
 
-Aggressive=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Aggressive = [
+    ("1", "calm", 1.0),
+    ("2", "pushy", 2.0),
+    ("3", "forceful", 3.0),
+    ("4", "hostile", 4.0),
+    ("5", "violent", 5.0)
+]
 
-Impulsive=[("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Impulsive = [
+    ("1", "cautious", 1.0),
+    ("2", "hasty", 2.0),
+    ("3", "reckless", 3.0),
+    ("4", "spontaneous", 4.0),
+    ("5", "unrestrained", 5.0)
+]
 
-Predictable=[
-    ("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Predictable = [
+    ("1", "erratic", 1.0),
+    ("2", "regular", 2.0),
+    ("3", "steady", 3.0),
+    ("4", "reliable", 4.0),
+    ("5", "consistent", 5.0)
+]
 
-Distracted=[
-    ("1"),
-    ("2"),
-    ("3"),
-    ("4"),
-    ("5"),]
+Distracted = [
+    ("1", "focused", 1.0),
+    ("2", "diverted", 2.0),
+    ("3", "preoccupied", 3.0),
+    ("4", "unfocused", 4.0),
+    ("5", "scattered", 5.0)
+]
+
 
 Abundance = [
     ("1", "Low", 0),
@@ -174,3 +206,32 @@ PredMamm = [
     ("4", "Often (1 to 3 times a month)", 3),
     ("5", "Very often (once a week or more)", 4)
 ]
+
+
+characteristics = {
+    "Sex": None,
+    "Age": None,
+    "Number": None,
+    "Home": None,
+    "Zone": None,
+    "Ext": None,
+    "Obs": None,
+    "Shy": None,
+    "Calm": None,
+    "Frightened": None,
+    "Intelligent": None,
+    "Vigilant": None,
+    "Persevering": None,
+    "Affectionate": None,
+    "Friendly": None,
+    "Solitary": None,
+    "Brutal": None,
+    "Dominant": None,
+    "Aggressive": None,
+    "Impulsive": None,
+    "Predictable": None,
+    "Distracted": None,
+    "Abundance": None,
+    "PredBird": None,
+    "PredMamm": None
+}
